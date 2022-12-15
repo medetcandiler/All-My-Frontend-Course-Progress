38a7e17c61986ad0714d04476021b106ac4c9366
Medetcan Diler <medetdiler1996@gmail.com>
2022-12-15 20:41:28 +0300

    My first Hello World

---
+++ JavaScript/first-step-into-js/hello-world.js
@@ -0,0 +1,8 @@
+// dokümana yazdırma 
+//  document.write("Hello World!");
+
+// console.log ile hello world  (ayrıca browserdan bu komutları verebiliriz.İnsert>Console)
+// console.log("Hello World!")
+
+// Uyarı Mesajı (Alert)
+// alert("Hello World!")
