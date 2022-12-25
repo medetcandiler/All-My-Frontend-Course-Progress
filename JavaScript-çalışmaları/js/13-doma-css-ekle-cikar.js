968a27d8d0a6c95840677c511f14989cc04f3e7f
Medetcan Diler <118726723+medetcandiler@users.noreply.github.com>
2022-12-25 18:56:59 +0300

    toggle, item(), replace, classlist add remove calismaları yapıldı

--- JavaScript-çalışmaları/js/13-doma-css-ekle-cikar.js
+++ JavaScript-çalışmaları/js/13-doma-css-ekle-cikar.js
@@ -1,27 +1,27 @@
 //************ css class eklemek ve cikartmak */
 
-let greeting=document.querySelector("#deneme")
+/*let greeting=document.querySelector("#deneme")
 console.log(greeting.classList)  // greeting id si olan h1 in classı varm ı yokmu ya baktık
 greeting.classList.add("text-primary") // class ekledik  
 greeting.classList.add("title")  // 2 adet classı ayrı ayrı eklememize gerek yok 
 greeting.classList.add("btn", "bg-secondary", "third-class")  // 3 clası tek classlist.add ile ekledik 
-console.log(greeting) // console yazdıırıp kontrol ettik ve eklendiğini gördük. 
+console.log(greeting) */// console yazdıırıp kontrol ettik ve eklendiğini gördük. 
 
 //peki içerisinden nasıl class sileceğiiz hadi öğrenelim ..
 
-console.log(greeting.classList.remove("third-class")) // third classı sildim..
+/*console.log(greeting.classList.remove("third-class"))*/ // third classı sildim..
 
 //eğer remova yaparken classı yanlış yazarsak sadece silmez hata vermez !!!!!!
 
 //mesela kullanıcı tıkladığında class ekliceğiz yada removelayacağız gibi fonksiyonlarla çalışacağız
 
 
-let midChild=document.querySelector("#list>li:nth-child(5)")
+/*let midChild=document.querySelector("#list>li:nth-child(5)")
 console.log(midChild.innerHTML)
 midChild.innerHTML="deneme"
 
 midChild.classList.add("txt-color","fs-xl")
-midChild.classList.remove("txt-color")
+midChild.classList.remove("txt-color")*/
 
 
 //todolist tekrar
@@ -34,3 +34,46 @@ midChild.classList.remove("txt-color")
 //     reachOl.append(newLi)
 //     reachTxt.value=""
 // }
+
+
+/*let addClass=document.querySelector("#list")
+addClass.classList.add("class-1", "class-2", "class-3","class-4")
+console.log(addClass)
+console.log(addClass.classList.length)
+addClass.classList.replace("class-2", "newclass-2")*/
+
+// let deneme=document.querySelector("#list")
+
+// let btn=document.querySelector("#btn").addEventListener("click", myFnc)
+// function myFnc(){
+//     deneme.classList.toggle("txt-color")}
+
+let myName=document.querySelector("#my-name")
+
+
+let addBtn=document.querySelector("#btn").addEventListener("click", addClass)
+function addClass(){
+    myName.classList.add("bg-orange")
+}
+let removeBtn=document.querySelector("#btn-2").addEventListener("click", removeClass)
+function removeClass(){
+    myName.classList.remove("bg-orange", "txt-transform")
+}
+
+function replaceClass(){
+    myName.classList.replace("bg-orange", "txt-transform")
+}
+
+function itemFnc(){
+    let firstItem=document.querySelector("#info")
+    let x=firstItem.classList.item(0)
+    alert(x)
+}
+
+
+
+
+
+
+
+
