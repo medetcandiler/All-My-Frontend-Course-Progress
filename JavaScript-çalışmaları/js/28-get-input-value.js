14941a1bd030065941d6e4df207f8bdd9a24af0c
Medetcan Diler <118726723+medetcandiler@users.noreply.github.com>
2023-01-08 21:59:48 +0300

    username password form with alerts

--- JavaScript-çalışmaları/js/28-get-input-value.js
+++ JavaScript-çalışmaları/js/28-get-input-value.js
@@ -1,42 +1,52 @@
+let usernameDOM=document.querySelector('#username');
+let passwordDOM=document.querySelector('#password');
 let formDOM=document.querySelector('#form')
+let list=document.querySelector('#list')
+const ALERT=(className='warning', title, message)=>
+`
+<div class="alert alert-${className} alert-dismissible fade show" role="alert">
+<strong>${title}</strong> ${message}
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`
 
-let username;
-let password;
 
-let forUserName=document.querySelector('#fusername')
-let forPassword=document.querySelector('#fpassword')
-let listDOM=document.querySelector('#userList')
+
+const alertDOM=document.querySelector('#alert')
 
 formDOM.addEventListener('submit', (e)=>{
     e.preventDefault();
-    switch(true){
-        case(!(isNaN(username))):
-            alert('please write your username by using characters')
-            break;
-        case(username===undefined):
-            alert('please do not keep empty username')
-        case(password===undefined):
-            alert('please write your password')
-            break;
-        default:
-            addItem()
+    if(usernameDOM.value==""){
+        alertDOM.innerHTML= ALERT('danger', 'empty username', 'please write sth in username input area')
+    }else if(passwordDOM.value==''){
+        alertDOM.innerHTML=ALERT('danger', 'empty psw', 'wrte psswrd')
+    }else if(usernameDOM.value.length<5){
+        alertDOM.innerHTML=ALERT('warning', 'less than 5 username' , 'write atleast 5 chcrt username' )
+    }else if(passwordDOM.value.length<5){
+        alertDOM.innerHTML=ALERT('warning', 'less than 5 achrc  pass ', ' at least 5 password')
+    }
+    else if(usernameDOM && passwordDOM){
+        alertDOM.innerHTML= ALERT('success', 'succesfully you managed to enter','congratulations')
+        addLi(usernameDOM.value, passwordDOM.value)
+        usernameDOM.value=''
+        passwordDOM.value=''
     }
 })
 
-function addItem(){
-    let newLi= document.createElement('li')
-    newLi.innerHTML= `${username} ${password}`
-    newLi.classList.add('list-group-item','d-flex','justify-content-between','align-items-start')
-    listDOM.append(newLi)
-}
 
-
-function changeFnc(event){
-    if(event.target.id==='username' && username!==''){
-        username=event.target.value
-    }else if (event.target.id==='password' && password!==''){
-        password=event.target.value
-    }else{
-        alert('please do not keep empty input areas')
+function handleChange(event){
+    if(event.target.name==='username'){
+        usernameDOM.value
+    }else if(event.target.name==='password'){
+        passwordDOM.value
     }
 }
+
+function addLi(username,password){
+    let newLi=document.createElement('li')
+    newLi.innerHTML= `username: ${username} <br> password:${password}`
+    newLi.classList.add('list-group-item')
+    list.append(newLi)
+}
+
+
