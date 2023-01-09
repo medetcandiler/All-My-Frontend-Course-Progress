b59be3f495d65c7bea7b5c07a7294181444ce83e
Medetcan Diler <118726723+medetcandiler@users.noreply.github.com>
2023-01-09 23:53:46 +0300

    create an array and reach array elements

--- JavaScript-çalışmaları/js/30-create-array-and-reach-array-elements.js
+++ JavaScript-çalışmaları/js/30-create-array-and-reach-array-elements.js
@@ -12,23 +12,108 @@ console.log(myArr)
 
 let mySecondArr=[5, 6, 7, 8]
 let newArray=myArr.concat(mySecondArr)
-console.log(newArray)
+console.log('concat',newArray)
+newArray+=[domain, isActive]
+console.log('+=',newArray)
+newArray=[domain]
+document.querySelector('#info').innerHTML= newArray
 
-let emptyArr=[] // empty list
-let x=emptyArr.concat(['denemek', 'guseldir'])
-console.log(x)
+let username='medetcan diler'
+console.log('split method', username.split())
 
+
+const reverseStr=(str)=>{
+    let stringToArr=str.split('') // splits a string into array
+    let reverseArr=stringToArr.reverse()  // reverse an array 
+    let arrtoStr=reverseArr.join('') // returns an array as string
+    return arrtoStr
+}
+console.log(reverseStr('aybiis'))
+
+let items=['deneme', 'array', 'olusturuldu']
+let newItems=items.concat(username)
+console.log(newItems,'-----')
+//document.querySelector('#info').innerHTML= newItems+'====='+ newItems.length
+
+let arrToString=newItems.join()
+console.log(arrToString, typeof arrToString)
+
+
+let emptyArr=[];
+let addItems=emptyArr.concat('deneme  yaptiim')
+//document.querySelector('#info').innerHTML+= addItems
+
+let x=[1, 2, 3, 4, 5, 6, 7]
 // array icindeki elemen/ oge sayisini olcmek
 console.log(x.length)
 
 // document.querySelector('#info').innerHTML+=' '+ x.length
+
+//*********** call the first element of array
 const firstElement=myArr[0]
-document.querySelector('#info').innerHTML= firstElement
+console.log(firstElement)
+// document.querySelector('#info').innerHTML= firstElement
 
-// let fname='medetcan'
-// let surName='diler'
 
-// let camelName= fname.charAt(0).toUpperCase()+ fname.slice(1)
-// document.querySelector('#info').innerHTML=camelName
+//************ call the last element of array */
+console.log(x.slice(-1), typeof x )
+console.log('call the last element of array', x[x.length-1])
+
+//******** call the mid element of array */
+x=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log('reach the middle element of array', x[Math.floor(x.length/2)])
+
+/******** degisken icindeki bilginin array olup olmadigi kontrolu  */
+console.log('checking if it is an array ', typeof x)  // object olarak gosteriyorr.isimize yaramadi
+
+console.log(Array.isArray(x)) // (x array.isArray) if x is an array it returns true.
+
+//************* Hangileri isArray -> returns true */
+
+console.log('[] :',Array.isArray([]))
+console.log('1 :' ,Array.isArray(1))
+console.log('true :' ,Array.isArray(true))
+
+
+
+
+// let name='medetcan'
+// let email='medetdiler@gmail.com'
+
+// let DOMAIN=email.slice(email.indexOf('@')+1)
+// document.querySelector('#info').innerHTML= DOMAIN
+
+
+
+let arr=[1, 2, ['bir', 'iki', 'uc'], 6]
+
+for(let i=0; i < arr.length ; i++){
+    console.log(arr[i],'for loop', typeof arr)
+}
+console.log(arr[2][3])  // array icindeki arraya ulasma yontemi [icerdeki arrayin index sayisi][icerdeki arrayin kacinci elemaninin indexi]
+
+let denemeArr=[1, 2, 3, 4, 5 ,6]
+// denemeArr.forEach((item, index)=> {
+//     console.log(item)
+// })
+
+let myCars=['bmw', 'toyota', 'suzuki', [1, 2, 3, 4]]
+let text=''
+// myCars.forEach(myFnc)
+// function myFnc(item, index){
+//     text += index + ': ' + item + '<br>'
+// }
+myCars.forEach((items, index) => {
+    text+=index+': ' +items + '<br>'
+});
+document.querySelector('#info2').innerHTML= text
+
+let months=['january', 'februrary', 'march', 'april', 'may ']
+console.log(months[months.length-1]) // last element of months array
+console.log(months[0]) // first element of months array
+console.log(months[Math.floor(months.length/2)])
+
+
+
 
 
