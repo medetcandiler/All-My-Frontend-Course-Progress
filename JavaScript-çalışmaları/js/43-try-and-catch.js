ed269e668bd28f714538be48b388bf2c0d5c5643
Medetcan Diler <118726723+medetcandiler@users.noreply.github.com>
2023-01-23 00:36:45 +0300

    and of the day

--- JavaScript-çalışmaları/js/43-try-and-catch.js
+++ JavaScript-çalışmaları/js/43-try-and-catch.js
@@ -1,16 +1,21 @@
 // How try and catch use in js
 
+//SYNTAX
+// try {
+//     //Çalıştırılacak kodlar
+//   }catch(hata) {
+//     //Hata yakalandığında çalışacak kodlar
+//   }finally {
+//     //try tamamlandıktan sonra hata olsada olmasada çalışacak olan kodlar.
+//   }
+
+
 let items;
 
 try{
-    items.forEach(item=> console.log(item*5))
-}catch(error){
-    console.log('myError:', error);
-    items=[4, 5, 6, 7, 3]
-    items.sort()
-    let maxItem= Math.max(...items)
-    let getRidOf= items.filter(e=> e != maxItem) 
-    console.log(getRidOf)
+    items.forEach(item=> console.log(item*5));
+}catch(err){ // exception
+    console.log('myError:', err);
 }
 
 console.log('the error management is working well ');
@@ -36,23 +41,45 @@ myPromise.then(
     }
 )
 
-const mySecondPromise= new Promise((resolve, reject)=>{
-    let isActive=1
-    setTimeout(() => {
-        if(!isNaN(isActive)){
-            resolve(1)
-        }else reject('yanlis varr ama sen bilirsin ')
-    }, 3000);
-}).then(
-    function(value){
-        let news = value*3;
-        return news
-    },
-    function(rejected){
-        console.log(rejected)
-        return rejected
+
+// const mySecondPromise= new Promise((resolve, reject)=>{
+//     let isActive=1
+//     setTimeout(() => {
+//         if(!isNaN(isActive)){
+//             resolve(1)
+//         }else reject('yanlis varr ama sen bilirsin ')
+//     }, 3000);
+// }).then(
+//     function(value){
+//         let news = value*3;
+//         return news
+//     },
+//     function(rejected){
+//         console.log(rejected)
+//         return rejected
+//     }
+// ).then(v=>{
+//     let x=v *10;
+//     return x;
+// }).then(success=> document.querySelector('#demo2').innerHTML=success + 'last step')
+
+
+// input sample 
+
+let formDOM=document.querySelector('#form').addEventListener('submit', myFnc)
+function myFnc(event){
+    event.preventDefault()
+    let writeDOM=document.querySelector('#demo2');
+    writeDOM.innerHTML=''
+    let inputDOM=document.querySelector('#age')
+    
+    try{
+        if(inputDOM.value.trim()== '')throw 'write sth'
+        if(isNaN(inputDOM.value)) throw 'write number'
+        if(inputDOM.value<5) throw 'too low'
+        if(inputDOM.value>10) throw 'quiet big'
+    }catch(e){
+        writeDOM.innerHTML= e
     }
-).then(v=>{
-    let x=v *10;
-    return x;
-}).then(success=> document.querySelector('#demo2').innerHTML=success + 'last step')
+    inputDOM.value=''
+}
