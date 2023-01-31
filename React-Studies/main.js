4f95e351f4c8930acbf74c4fa5fe0a1c13c841fe
Medetcan Diler <medetdiler1996@gmail.com>
2023-01-31 20:53:23 +0300

    ES6 module systems studied

---
+++ React-Studies/main.js
@@ -0,0 +1,7 @@
+//let slugify = require('slugify'); // it is not working because we add on package.json file "type": "module". What with the changes we need to use import syntax instead of require..
+
+//import slugify from 'slugify'  // ES6 module import syntax.
+
+import Topla from "./my-modue.js"
+
+console.log(sum(5, 6))
