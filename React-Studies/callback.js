633ef845192f24bd7049e76cbba031d016817710
Medetcan Diler <medetdiler1996@gmail.com>
2023-02-01 21:16:16 +0300

    react | promises studied

--- React-Studies/callback.js
+++ React-Studies/callback.js
@@ -33,8 +33,121 @@
 // getData()
 
 
-( async() => {
-    const Data= await (await fetch('https://jsonplaceholder.typicode.com/posts/1')).json();
-    const Data2= await (await fetch('https://jsonplaceholder.typicode.com/posts/2')).json()
-    console.log('first data : ', Data, 'second data : ', Data2)
-})();
+
+
+
+// function tryingCallBack(param){
+//     param()
+// };
+// tryingCallBack(function add(){
+//     console.log('callback function is successfull executed')
+// });
+
+import axios from 'axios';
+
+// ( async() => {
+//     const { data:post1 } = await axios('https://jsonplaceholder.typicode.com/posts/1');
+//     const { data:post2 } = await axios('https://jsonplaceholder.typicode.com/posts/2')
+//     console.log(post1 , post2)
+// })();
+
+// async function getData(){
+//     const { data: post1 } = await axios('https://jsonplaceholder.typicode.com/posts/3');
+//     const { data: post2 } = await axios('https://jsonplaceholder.typicode.com/posts/4');
+//     console.log(post1, post2);
+// }
+// getData()
+
+
+// function cookPasta(){
+//     let makarna= false;
+//     return new Promise(function promise(resolve, reject){
+//         if(makarna){
+//             resolve('pasta exists on the top shelf. Get that and prepare it.');
+//         }else {
+//             reject('pasta does not exist. You need to buy pasta');
+//         }
+//     })
+// }
+// cookPasta()
+// .then( prepare => {
+//     console.log(prepare);
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() =>{
+//             if(prepare){
+//                 resolve('pasta is preparing and almost ready to eat wait for a while');
+//             }else reject('pasta is not preparing. you need to think of an another food');
+//     }, 3000 )
+//     })
+// }).catch( err => console.log(err)
+// ).then(pasta => {
+//     console.log(pasta);
+//     return new Promise((resolve, reject) => {
+//         if(pasta){
+//             resolve('pasta is ready to eat');
+//         }else reject('make sandwich');
+//     })
+// }).catch(err => console.log(err)
+// ).then(result =>{
+//     console.log(result);
+// } 
+// ).finally(() => console.log('you can sleep fully'));
+
+
+// let myPromise = function(number){
+//     return new Promise(function(resolve, reject){
+//         if ( number > 10 ){
+//             resolve('true')
+//         }else reject('false')
+//     })
+// }
+// myPromise(22).then(
+//     (data) => console.log(data, ' data is accepted '),
+//     (rejected)=> console.log(rejected, 'rejected')
+// )
+
+const getPosts = function(){
+    return new Promise(async (resolve, rejected) => {
+    const { data } = await axios('https://jsonplaceholder.typicode.com/posts/3');
+    if(data){
+        resolve(data)
+    }else rejected('data is not here')
+})}
+
+const getPost2 = function(){
+    return new Promise(async(resolve, rejected) => {
+        const { data } = await axios('https://jsonplaceholder.typicode.com/posts/5');
+        resolve(data)
+})}
+
+
+
+( async()=>{
+    const user= await getPosts()
+
+    const post = await getPost2()
+    
+    console.log(post, user)
+})()
+
+// ( async() => {
+//     const Data= await (await fetch('https://jsonplaceholder.typicode.com/posts/1')).json();
+//     const Data2= await (await fetch('https://jsonplaceholder.typicode.com/posts/2')).json()
+//     console.log('first data : ', Data, 'second data : ', Data2)
+// })();
+// let asyncFnc= async function(){
+//     try{
+//         const user = await getPost2()
+//         const post = await getPosts()
+//         console.log(user, post)
+//     }catch(err){
+//         console.log(err)
+//     }
+    
+// }
+// asyncFnc()
+
+// Promise.all([getPosts(), getPost2()]).then(
+//     (success) => console.log(success),
+//     (err) => console.log(err)
+// )
