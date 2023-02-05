757c1a234d56bac8dae7892a672253d5ea27a093
Medetcan Diler <medetdiler1996@gmail.com>
2023-02-06 00:21:27 +0300

    Default Props has been studied, and recapped all props-section

--- React-Studies/components/my-first-react-app/src/App.js
+++ React-Studies/components/my-first-react-app/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
 // import {Header ,Indicator, myPersonalInfo, isLoggedIn} from './newComponents/header'
-import {comment, Comment, formatDate} from './newComponents/hello-kitty'
+import Car from './newComponents/props'
 import User from './newComponents/user'
 
 
 const address={
     title: 'Kartal/Istanbul',
-    zip: 24334,
+    zip: 34862,
     country: 'Turkiye'
 }
 const friends= [
@@ -26,21 +26,27 @@ const friends= [
 
 ]
 
+const car={
+    cars: ['toyota', 'mercedes', 'bmw'],
+    model: 'true'
+}
+const numbers=[30, 40, 50, 60, 100]
+
 function App(){
     return(
         <React.Fragment>
-            <Comment 
-                date={comment.date}
-                text ={comment.text}
-                author={comment.author}
-            />
             <User 
-            fname='medetcan'
+            // fname='medetcan'
             lname='diler'
             age={'27'}
-            isLoggedIn={true}
             address={address}
             friends={friends}
+            />
+            <Car 
+            owner={false}
+            car={car}
+            score={numbers}
+            // isCarExist={false}
             />
         </React.Fragment>
     );
--- React-Studies/components/my-first-react-app/src/newComponents/user.js
+++ React-Studies/components/my-first-react-app/src/newComponents/user.js
@@ -1,22 +1,26 @@
 import PropTypes, { object, oneOfType } from "prop-types";
-function User( {fname ,lname, age, isLoggedIn, friends, address} ){
+
+function User( { fname,lname, age, isLoggedIn, friends, address} ){
+    if(!isLoggedIn){
+       return (
+            <>
+                <h1>the user cannot find please try again..</h1>
+            </>
+       )
+    }
     return (
-        <div>
-            <p className="lead">
+        <>
+            <h2>
                 {
-                    isLoggedIn ? `I'm ${fname} ${lname}. I'm ${age} years old. I live in ${address.title} ${address.zip}` : 'you cannot enter please try again '
+                    ` User informations => name:${fname} surname:${lname} age:${age}`
                 }
-            </p>
-            <div className="lead">
-            {
-                friends.map( (element) => {
-                    return <div key={element.id} >
-                        {element.name}
-                    </div>
-                })
-            }
+            </h2>
+            <div>
+                {`The users addresses informations => The user lives in ${address.country} and ${address.title} and also zip number of the user is ${address.zip}`}
             </div>
-        </div>
+            <br />
+            {friends.map( item => <div key={item.id}> {item.name}</div>)}
+        </>
 
     )
 }
@@ -25,13 +29,18 @@ User.propTypes= {
     fname: PropTypes.string.isRequired,
     lname: PropTypes.string.isRequired,
     age: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    isLoggedIn: PropTypes.bool,
     friends: PropTypes.array,
+    isLoggedIn: oneOfType([PropTypes.string, PropTypes.bool]),
     address: PropTypes.shape({
         title: PropTypes.string,
         country: PropTypes.string,
-        zip: PropTypes.number
+        zip: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
     })
+}
+
+User.defaultProps = {
+    isLoggedIn: true,
+    fname: 'isimsiz'
 }
 
 export default User;
