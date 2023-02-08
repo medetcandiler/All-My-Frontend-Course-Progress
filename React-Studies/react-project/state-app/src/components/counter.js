6489bb4f034d802e95ab8b754465151128d74556
Medetcan Diler <medetdiler1996@gmail.com>
2023-02-08 21:13:40 +0300

    states has been recapped

--- React-Studies/react-project/state-app/src/components/counter.js
+++ React-Studies/react-project/state-app/src/components/counter.js
@@ -2,14 +2,11 @@ import {useState} from 'react';
 
 export default function Counter(){
     const[counter, setCounter] = useState(0)
-    let increaser = ()=> setCounter(counter + 1);
-    let decreaser = ()=> setCounter( counter - 1);
     return(
-        <>
-            <h1>Welcome to our counter app!</h1>
-            <h3>{counter }</h3>
-            <button onClick={increaser}>Start counter</button><br /><br />
-            <button onClick={decreaser}>Stop counter</button>
-        </>
+        <div className="container">
+            <h3>{counter}</h3>
+            <button onClick={ ()=> setCounter(counter + 1) }>increase</button>
+            <button onClick={ ()=> setCounter( counter - 1)}>decrease</button>
+        </div>
     )
 }
--- React-Studies/react-project/state-app/src/components/form-sample.js
+++ React-Studies/react-project/state-app/src/components/form-sample.js
@@ -1,18 +1,29 @@
 import {useState} from 'react';
 
 export default function Form(){
-    const[firstName, setFirstName] = useState('')
-    const[lastName, setLastName] = useState('')
+    const[form, setForm] = useState({firstName: 'empty' , lastName:'empty'})
+
+    function changeValue(e){
+        setForm({ [e.target.name]:e.target.value})
+    }
+
+    // const[lastName, setLastName] = useState('empty')
+    // function changeFname(e){
+    //     setFirstName(e.target.value)
+    // }
+    // function changeLname(e){
+    //     setLastName(e.target.value)
+    // }
     return (
         <>
-            <form action="#" onSubmit={(e) => e.preventDefault()}>
-                <input type="text" placeholder="First Name" value={firstName} onChange={(e)=> setFirstName(e.target.value) } />
-                <input type="text" placeholder="Last Name" value={lastName}  onChange={(e) => setLastName(e.target.value)}/>
-                <h1>Hi {firstName} {lastName}</h1>
-                <button onClick={() => {
-                    setFirstName('');
-                    setLastName('')
-                }} >Reset</button>
+            <form action="#" onSubmit={ () => setForm('')}>
+                <label htmlFor="fname">First Name: </label>
+                <input onChange={changeValue} type="text" name="firstName" value={form.firstName} id="fname" /><br />
+                <label htmlFor="lname">Last Name: </label>
+                <input onChange={changeValue}  type="text" name="lastName" id="lname" value={form.lastName} /><br />
+                <input type="submit" value="RESET" />
+                <h3>Your First Name: <i>{form.firstName}</i> </h3>
+                <h3>Your Last Name: <i>{form.lastName}</i> </h3>
             </form>
         </>
     )
