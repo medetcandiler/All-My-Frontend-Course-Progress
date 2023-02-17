9867a97fc3877b8bcd011c41283d35c16547326e
Medetcan Diler <medetdiler1996@gmail.com>
2023-02-17 17:34:55 +0300

    started to learn react-router | how to implement react-router | basic level of router sample is done

--- React-Studies/react-project/my-app/src/App.js
+++ React-Studies/react-project/my-app/src/App.js
@@ -1,12 +1,42 @@
 import './App.css';
-import Users from './components/users/users'
+import React from "react";
+import Home from './components/Home/home';
+import About from './components/about-us/about-us';
+import Users from './components/users/users';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
 
 function App() {
     return (
-        <div className="container">
-            <Users />
-        </div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link className='link' to="/">Home</Link>
+                            {/* <a href="/Home">Home</a> */}
+                        </li>
+                        <li>
+                            <Link className='link' to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link className='link' to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/users" element={<Users />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
