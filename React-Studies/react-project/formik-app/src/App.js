e7efd950871a5370eaf065736dbc4c76b2ed0b4b
Medetcan Diler <medetdiler1996@gmail.com>
2023-02-20 15:44:50 +0300

    react formik studied

---
+++ React-Studies/react-project/formik-app/src/App.js
@@ -0,0 +1,81 @@
+import './App.css';
+import { Formik, useFormik } from 'formik';
+
+
+
+const validate = values => {
+    const errors = {};
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+        errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if (!values.lastName) {
+        errors.lastName = 'Required';
+    } else if (values.lastName.length > 20) {
+        errors.lastName = 'Must be 20 characters or less';
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+    }
+
+    return errors;
+};
+
+
+function App() {
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: ''
+        },
+        validate,
+        onSubmit: values => console.log(values)
+    })
+
+    return (
+        <div className="App">
+            <br /><br />
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="firstName">First Name</label>
+                <input
+                    id="firstName"
+                    name="firstName"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.firstName}
+                />
+                {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+                <br /><br />
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                    id="lastName"
+                    name="lastName"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.lastName}
+                />
+                {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+                <br /><br />
+                <label htmlFor="email">Email Address</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+                {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                <br /><br />
+                <button type="submit">Submit</button>
+            </form>
+        </div >
+    );
+}
+
+export default App;
